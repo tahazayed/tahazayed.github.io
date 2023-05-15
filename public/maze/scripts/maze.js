82df90197b024d5f13587afad0483e0d9da11d80
Taha Elsayed <tahazayed@gmail.com>
2023-05-14 23:51:30 -0500

    Add Maze

---
+++ public/maze/scripts/maze.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    let lost = false;
+    let startClicked = false;
+
+    $('.boundary').each((idx, e) => {
+        $(e).mouseover(() => { if (startClicked) youLose(); });
+    });
+
+    $('#start').on("click", () => {
+        lost = false;
+        startClicked = true;
+        $('.boundary').removeClass("youlose");
+    })
+
+
+    $('#end').on("mouseover", () => {
+        if (!lost && startClicked) {
+            $('#status').text("You Win");
+            startClicked = false;
+        }
+    })
+
+    $('#maze').on("mouseleave", () => {
+        if (startClicked) {
+            youLose();
+        }
+    });
+
+});
+
+function youLose() {
+    $('.boundary').addClass("youlose")
+    lost = true;
+    $('#status').text("Sorry, you lose");
+    startClicked = false;
+}
