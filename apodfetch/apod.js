c951f15b06fbab12efac5b352d4b126a5110d5f5
Taha Elsayed <tahazayed@gmail.com>
2023-05-19 13:44:18 -0500

    remove unused file

---
+++ apodfetch/apod.js
@@ -0,0 +1,27 @@
+$(document).ready(
+    function () {
+        $("#view_button").click(getPicture);
+    });
+
+function getPicture() {
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod",
+        type: "GET",
+        data: {
+            api_key: "HAI8yceW0yUSWNtQv938yAgC5EyMB10w2iK6hyNx",
+            date: $("#date").val()
+        },
+        dataType: "json",
+        "success": showPicture,
+        "error": noPicture
+    });
+};
+
+function showPicture(data) {
+    $("#pic").attr("src", data.url);
+    $("h2.title").text(data.title);
+};
+
+function noPicture(error) {
+    alert(error.responseText);
+};
