3e61f89c0ccb7e7ede302e929b91a1939b94b680
Taha Elsayed <tahazayed@gmail.com>
2023-05-20 11:47:42 -0500

    using fetch

--- apodfetch/apod.js
+++ apodfetch/apod.js
@@ -2,6 +2,8 @@ window.onload = function () {
     const viewButton = document.getElementById("view_button");
     viewButton.onclick = getPicture;
 
+    const dateText = document.getElementById("date");
+
     function getPicture() {
 
         // encode to scape spaces
@@ -9,10 +11,13 @@ window.onload = function () {
         const url = 'https://api.nasa.gov/planetary/apod?';
         const params = {
             api_key: "HAI8yceW0yUSWNtQv938yAgC5EyMB10w2iK6hyNx",
-            date: $("#date").val()
+            date: dateText.value
         }
         // this line takes the params object and builds the query string
-        const query = Object.keys(params).map(k => `${esc(k)}=${esc(params[k])}`).join('&');
+        const query = Object.keys(params)
+            .map(k => `${esc(k)}=${esc(params[k])}`)
+            .join('&');
+
         fetch(url + query)
             .then(response => response.json())
             .then(data => showPicture(data))
